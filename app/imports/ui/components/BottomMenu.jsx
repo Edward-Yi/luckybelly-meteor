d2027f7b639fc5c4425a5db900c68aec47a01594
Edward Yi <42788217+Edward-Yi@users.noreply.github.com>
2018-10-18 12:23:03 -1000

    Finish

---
+++ app/imports/ui/components/BottomMenu.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Image, Grid, List } from 'semantic-ui-react';
+
+class BottomMenu extends React.Component {
+  render() {
+    return (
+        <div className="footer-background">
+          <Grid container columns="three">
+            <Grid.Column>
+              <Image src="http://luckybelly.com/img/lucky-logo-light.png"/>
+            </Grid.Column>
+            <Grid.Column>
+              Links
+              <hr/>
+              <List>
+                <List.Item>Hours</List.Item>
+                <List.Item>Menus</List.Item>
+                <List.Item>Reservations</List.Item>
+                <List.Item>Corkage</List.Item>
+                <List.Item>Contact</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column>
+              Contact
+              <hr/>
+              <List>
+                <List.Item>808-531-1888</List.Item>
+                <List.Item>eat@luckybelly.com</List.Item>
+                <List.Item>50 N. Hotel St. Honolulu, HI 96817</List.Item>
+              </List>
+            </Grid.Column>
+          </Grid>
+        </div>
+    )
+  }
+}
+
+export default BottomMenu;
---
+++ app/imports/ui/components/Middle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+
+
+class Middle extends React.Component {
+  render() {
+    return (
+        <Image fluid src="http://luckybelly.com/img/bellybowl.jpg"/>
+    );
+  }
+}
+
+export default Middle;
